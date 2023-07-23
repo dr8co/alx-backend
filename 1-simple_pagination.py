87b350c377f6434070e5881fbfadb33e39b4c34c
Ian Duncan <76043277+dr8co@users.noreply.github.com>
2023-07-23 21:04:38 +0300

    More pagination

---
+++ 1-simple_pagination.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python3
+"""
+A simple helper function.
+"""
+
+from typing import Tuple, List
+
+
+def index_range(page: int, page_size: int) -> Tuple[int, int]:
+    """
+    Return a tuple of size two containing a start index and an end index
+    corresponding to the range of indexes to return in a list for those
+    particular pagination parameters.
+    """
+    start_index = (page - 1) * page_size
+    end_index = page * page_size
+    return (start_index, end_index)
+
+
+class Server:
+    """
+    Server class to paginate a database of popular baby names.
+    """
+
+    DATA_FILE = "Popular_Baby_Names.csv"
+
+    def __init__(self):
+        """
+        Initialize the class.
+        """
+        self.__dataset = None
+
+    def get_dataset(self) -> List[List]:
+        """
+        cached dataset
+        """
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                data = f.read()
+            data = data.split("\n")
+            data = [row.split(",") for row in data]
+            self.__dataset = data
+        return self.__dataset
+
+    def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+        """
+        Get page from dataset
+        """
+        assert isinstance(page, int) and page > 0
+        assert isinstance(page_size, int) and page_size > 0
+        self.get_dataset()
+        start, end = index_range(page, page_size)
+        return self.__dataset[start:end]
